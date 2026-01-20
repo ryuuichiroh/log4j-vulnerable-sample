d79f0f48aa34bf24469bf36654fbefed0825ce5b
ryuuichiroh <ryuuichiroh.nakayama@jp.ricoh.com>
2026-01-20 23:14:17 +0900

    Init

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.apache.log4j.Logger;
+
+public class App {
+    private static final Logger logger = Logger.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello, this is a sample application using Log4j 1.2.17");
+    }
+}
