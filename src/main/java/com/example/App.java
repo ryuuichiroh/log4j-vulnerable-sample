44a4dcf73bf03b0f8e20c16979464a2b304c702b
Ryuichiro Nakayama <67630868+ryuuichiroh@users.noreply.github.com>
2026-02-08 09:35:06 +0000

    検証:1.1.3を使うように修正 (#11)

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,11 @@
 package com.example;
 
-// import org.apache.log4j.Category; // 1.1.3 Category import
-import org.apache.log4j.Logger; // 1.2.17 Logger import
+import org.apache.log4j.Category; // 1.1.3 Category import
+// import org.apache.log4j.Logger; // 1.2.17 Logger import
 
 public class App {
-    // private static Category logger = Category.getInstance(App.class);
-    private static final Logger logger = Logger.getLogger(App.class);
+    private static Category logger = Category.getInstance(App.class);
+    // private static final Logger logger = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
         logger.info("Hello, this is a sample application using Log4j.");
